c0e2f0f7c8acf22c0121887be507f8b6e5040e4d
choiyounghoon <cyh0214@gmail.com>
2025-01-21 09:48:55 +0900

    add test code

--- src/test/java/com/example/app/AppApplicationTests.java
+++ src/test/java/com/example/app/AppApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest
 class AppApplicationTests {
 
+	@Autowired
+	MockMvc mock;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mock.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"))
+			.andExpect(model().attribute("name", "Spring Boot"))
+			.andExpect(content().string(containsString("Spring Boot")));
 	}
 
 }
